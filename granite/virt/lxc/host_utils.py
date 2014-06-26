68f22e66f572635bd9a9f382b9d1138c84712c77
Chuck Short <chuck.short@canonical.com>
2014-06-25 20:33:53 -0400

    More host changes
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

---
+++ granite/virt/lxc/host_utils.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2014 Canonical Ltd
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from oslo.config import cfg
+import lxc
+
+from nova.openstack.common.gettextutils import _   # noqa
+from nova.openstack.common import jsonutils
+from nova.openstack.common import log as logging
+from nova import utils
+
+CONF = cfg.CONF
+
+log = logging.getLogger(__name__)
+
+
+def get_memory_info():
+    out = open('/proc/meminfo')
+    for line in out:
+        if 'MemTotal:' == line.split()[0]:
+            split = line.split()
+            total  = float(split[1])
+        if 'MemFree:' == line.split()[0]:
+            split = line.split()
+            free  = float(split[1])
+         if 'Buffers:' == line.split()[0]:
+            split = line.split()
+            buffers = float(split[1])
+         if 'Cached:' == line.split()[0]:
+            split = line.split()
+            cached = float(split[1])
+    close('/proc/meminfo')
+    used = (total - (free + buffers + cached)
+    return {'total': int(total/1024)
+            'free': int(free/1024)
+            'used': int(used/1024)}
+
+def get_disk_info():
+    hddinfo = os.statvfs(CONF.instances_path)
+    total = hddinfo.f_frsize * hddinfo.f_blocks
+    free = hddinfo.f_frsize * hddinfo.f_bavail
+    used = hddinfo.f_frsize * (hddinfo.f_blocks - hddinfo.f_bfree)
+    return {'total': total, 'free': free, 'used': used}
--- granite/virt/lxc/hostops.py
+++ granite/virt/lxc/hostops.py
@@ -12,6 +12,23 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import os
+
+from oslo.config import cfg
+import lxc
+
+from granite.virt.lxc import host_utils as host_utils
+from nova.openstack.common.gettextutils import _   # noqa
+from nova.openstack.common import jsonutils
+from nova.openstack.common import log as logging
+from nova.openstack.common import units
+from nova import utils
+
+CONF = cfg.CONF
+
+log = logging.getLogger(__name__)
+
+
 class HostOps(object):
     def __init__(self):
         selt._stats = None
@@ -22,7 +39,22 @@ class HostOps(object):
         return self._status
 
     def get_available_resource(self, nodename):
-        pass
+        dic = {'vcpus': 1,
+               'memory_mb': 8192,
+               'local_gb': 1028,
+               'vcpus_used': 0,
+               'memory_mb_used': 0,
+               'local_gb_used': 0,
+               'hypervisor_type': 'lxc',
+               'hypervisor_verison': utils.convert_version_to_string(lxc.version),
+               'hypervisor_hostname' CONF.host,
+               'disk_available_least': 0,
+               'cpu_info': '?',
+               'supported_instances': jsonutils.dumps([
+                                     ('i686', 'lxc', 'lxc'),
+                                     ('x86_64', 'lxc', 'lxc'),
+                                     ])
+        return dic
 
     def _update_status(self):
         pass
