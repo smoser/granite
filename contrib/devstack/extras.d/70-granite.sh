8a1eb94037d4ea3554489596c4c06fac9c821ec4
Chuck Short <chuck.short@canonical.com>
2014-06-30 13:22:42 -0400

    Add devstack support.
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

---
+++ contrib/devstack/extras.d/70-granite.sh
@@ -0,0 +1,34 @@
+# granite.sh - Devstack extras script to install native lxc
+
+if [[ $VIRT_DRIVER == "docker" ]]; then
+
+	if [[ $1 == "source" ]]; then
+
+    # Keep track of the current directory
+    SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
+    TOP_DIR=$SCRIPT_DIR
+
+    echo $SCRIPT_DIR $TOP_DIR
+
+    # Import common functions
+    source $TOP_DIR/functions
+
+    # Load local configuration
+    source $TOP_DIR/stackrc
+
+    FILES=$TOP_DIR/files
+
+    # Get our defaults
+    source $TOP_DIR/lib/nova_plugins/hypervisor-granite
+
+	elif [[ $2 == "install" ]] ; then
+		if is_ubuntu; then
+			install_package --force-yes lxc lxc-dev
+
+			git clone https://github.com/lxc/python2-lxc && \
+				python setup.py build && python setup.py install
+		fi
+
+	fi
+
+fi
---
+++ contrib/devstack/prepare_devstack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xe
+
+env
+
+NOVAGRANITEDIR=$(realpath $(dirname $0)/../..)
+INSTALLDIR=${INSTALLDIR:-/opt/stack}
+
+cp NOVAGRANTIEDIR/contrib/devstack/extras.d/70-granite.sh $INSTALLDIR/devstack/extras.d
+cp NOVAGRANTIEDIR/contrib/devstack/lib/nova_plugins/hypervisor-granite $INSTALLDIR/devstack/lib/nova_plugins/
