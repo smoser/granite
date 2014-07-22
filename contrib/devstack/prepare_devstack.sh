fbdcb132f835fe3a2d34288bcaed6d00e46a6982
Chuck Short <chuck.short@canonical.com>
2014-07-22 17:38:23 +0000

    Fix devstack granite
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- contrib/devstack/prepare_devstack.sh
+++ contrib/devstack/prepare_devstack.sh
@@ -7,9 +7,8 @@ env
 NOVAGRANITEDIR=$(realpath $(dirname $0)/../..)
 INSTALLDIR=${INSTALLDIR:-/opt/stack}
 
-cp $NOVAGRANTIEDIR/contrib/devstack/extras.d/70-granite.sh $INSTALLDIR/devstack/extras.d
-cp $NOVAGRANTIEDIR/contrib/devstack/lib/nova_plugins/hypervisor-granite $INSTALLDIR/devstack/lib/nova_plugins/
+cp $NOVAGRANITEDIR/contrib/devstack/extras.d/70-granite.sh $INSTALLDIR/devstack/extras.d
+cp $NOVAGRANITEDIR/contrib/devstack/lib/nova_plugins/hypervisor-granite $INSTALLDIR/devstack/lib/nova_plugins/
 cat - <<-EOF >> $INSTALLDIR/devstack/localrc
 export VIRT_DRIVER=granite
-export HOST_IP=$HOST_IP
 EOF
