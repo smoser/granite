7818408dedcfddd47b8cbfd35b0f84f95891e4a6
Chuck Short <chuck.short@canonical.com>
2014-07-22 15:59:14 -0400

    install granite in the right places
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- contrib/devstack/prepare_devstack.sh
+++ contrib/devstack/prepare_devstack.sh
@@ -9,6 +9,7 @@ INSTALLDIR=${INSTALLDIR:-/opt/stack}
 
 cp $NOVAGRANITEDIR/contrib/devstack/extras.d/70-granite.sh $INSTALLDIR/devstack/extras.d
 cp $NOVAGRANITEDIR/contrib/devstack/lib/nova_plugins/hypervisor-granite $INSTALLDIR/devstack/lib/nova_plugins/
+cp $NOVAGRANITEDIR/contrib/devstack/lib/granite $INSTALLDIR/devstack/lib/granite
 cat - <<-EOF >> $INSTALLDIR/devstack/localrc
 export VIRT_DRIVER=granite
 EOF
