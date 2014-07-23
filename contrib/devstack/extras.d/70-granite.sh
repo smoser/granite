7e03a8dcc5a60a7c1cfe848fb17959564233be0e
Chuck Short <chuck.short@canonical.com>
2014-07-22 20:33:20 -0400

    More granite fixes
    Signed-off-by: Chuck Short <chuck.short@canonical.com>

--- contrib/devstack/extras.d/70-granite.sh
+++ contrib/devstack/extras.d/70-granite.sh
@@ -1,35 +1,30 @@
 # granite.sh - Devstack extras script to install native lxc
 
 if [[ $VIRT_DRIVER == "granite" ]]; then
-
 	if [[ $1 == "source" ]]; then
+       # Keep track of the current directory
+       SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
+       TOP_DIR=$SCRIPT_DIR
 
-    # Keep track of the current directory
-    SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
-    TOP_DIR=$SCRIPT_DIR
+       echo $SCRIPT_DIR $TOP_DIR
 
-    echo $SCRIPT_DIR $TOP_DIR
+       # Import common functions
+       source $TOP_DIR/functions
 
-    # Import common functions
-    source $TOP_DIR/functions
+       # Load local configuration
+       source $TOP_DIR/stackrc
 
-    # Load local configuration
-    source $TOP_DIR/stackrc
+       FILES=$TOP_DIR/files
 
-    FILES=$TOP_DIR/files
+       # Get our defaults
+       source $TOP_DIR/lib/nova_plugins/hypervisor-granite
+	   source $TOP_DIR/lib/granite
+	   elif [[ $2 == "install" ]] ; then
+		  echo_summary "Configuring granite"
+		  if is_ubuntu; then
+		  	  install_package --force-yes lxc lxc-dev
 
-    # Get our defaults
-    source $TOP_DIR/lib/nova_plugins/hypervisor-granite
-	source $TOp_dir/lib/granite
-
-	elif [[ $2 == "install" ]] ; then
-		echo_summary "Configuring granite"
-		if is_ubuntu; then
-			install_package --force-yes lxc lxc-dev
-
-		fi
-		install_granite
-
+		  fi
+		  install_granite
 	fi
-
 fi
